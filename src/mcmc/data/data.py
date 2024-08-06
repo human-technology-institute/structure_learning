5e425a6f30f8c5d362f05021cc9fe887ef27b4ba
Kristofer delas Penas <kristofer.delaspenas@uts.edu.au>
2024-08-06 16:49:35 +1000

    lazy dataset loading

--- src/mcmc/data/data.py
+++ src/mcmc/data/data.py
@@ -1,9 +1,23 @@
 import os
+from collections import UserDict
+from typing import Any
 import pandas as pd
 from pathlib import Path
 
 path = Path(__file__).parent.absolute()
 
-datasets = {}
+class LazyDataset(UserDict):
 
-datasets['sachs'] = pd.read_excel(os.path.join(path, 'datafiles/sachs/1. cd3cd28.xls'))
+    def __getitem__(self, key: Any) -> Any:
+        item = super().__getitem__(key)
+        if type(item) == str: # data not read yet
+            item = pd.read_excel(os.path.join(path, item))
+            super().__setitem__(key, item)
+        return item
+
+    def __setitem__(self, key: Any, item: Any) -> None:
+        return super().__setitem__(key, item)
+
+datasets = LazyDataset()
+
+datasets['sachs'] = 'datafiles/sachs/1. cd3cd28.xls'
