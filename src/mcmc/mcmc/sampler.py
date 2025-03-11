b44b8974fdd4fcb5c8ff5efd8eaa08aafbbdcf7a
Gilad Francis <gilad.francis@gmail.com>
2025-03-11 14:18:42 +1100

    OPAD added as another sampler

--- src/mcmc/mcmc/sampler.py
+++ src/mcmc/mcmc/sampler.py
@@ -3,6 +3,7 @@ from abc import ABC, abstractmethod
 import numpy as np
 
 
+# TODO: logging
 class Sampler(ABC):
 
     def __init__(self, model, proposal, prior, **kwargs):
@@ -62,7 +63,7 @@ class Sampler(ABC):
 
     def postprocess(self):
         """
-        this function allows for specific pre and post processing of the run (e.g. removal of burn in_
+        this function allows for specific pre- and post-processing of the run (e.g. removal of burn in_
         Returns:
 
         """
@@ -109,5 +110,67 @@ class MCMCSampler(Sampler):
     def postprocess(self):
         return self._chain_thetas[self.burn_in:], self._chain_logpost[self.burn_in:]
 
+
 #####################################################################################################################
-#OPAD
+# OPAD
+class OPADSampler(Sampler):
+
+    def __init__(self, model, proposal, prior, **kwargs):
+        """
+
+        Args:
+            model:
+            proposal:
+            prior:
+            **kwargs:  sampler: an un-instatiated object of type Sampler.
+                       opad_plus_option: list of tuples or a dict, with the dict keys of additional samples states,
+                       for example [(theta1, lop_p1), (theta2, lop_p2)].
+
+        """
+
+        super().__init__(model, proposal, prior, **kwargs)
+
+        # OPAD uses another sampler to traverse the search space.
+        # This sampler must be of class Sampler
+        sampler = kwargs.get("sampler", None)
+        assert isinstance(sampler, Sampler)
+        self._sampler = sampler.__init__(model, proposal, prior, **kwargs)  # instatiating internal sampler
+
+        self.opad_plus_option = int(kwargs.get("opad_plus", None))
+        if isinstance(self.opad_plus_option, (list, tuple)):
+            self.opad_plus_option = {item[0]: item[1] for item in self.opad_plus_option}
+        elif not (self.opad_plus_option is None or isinstance(self.opad_plus_option, dict)):
+            raise ValueError("opad_plus: must be either None, list of tuples or a dictionary")
+        # self.opad_plus_option - converted to a dict
+
+        # Model containers
+        self.state_dict = {}
+        self.counter_dict = {}
+
+    def step(self):
+        """
+        """
+
+        # Return sample from the internal sampler, e.g. MCMC
+        res = self._sampler.step()
+
+        # Update OPAD dictionary of visited states and their scores
+        self.state_dict[res['theta']] = res['log_posterior']
+        if 'theta' in self.counter_dict.keys():
+            self.state_dict[res['theta']] += 1
+        else:
+            self.state_dict[res['theta']] = 1
+
+        # OPAD+ options
+        if self.opad_plus_option is not None:
+            for key2state, key2value in self.opad_plus_option.items():
+                self.state_dict[res[key2state]] = res[key2value]
+                if key2state in self.counter_dict.keys():
+                    self.state_dict[res[key2state]] += 1
+                else:
+                    self.state_dict[res[key2state]] = 1
+
+        return res
+
+    def postprocess(self):
+        return self.state_dict
