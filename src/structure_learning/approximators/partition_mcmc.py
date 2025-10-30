0e544724c36fdfb206ef02d1363c5b9ff160c049
Kristofer delas Penas <kristofer.delaspenas@uts.edu.au>
2025-10-30 15:47:43 +1100

    fixed bug on pmcmc when starting with blocklist (access adjacency matrix through incidence property of graph class)

--- src/structure_learning/approximators/partition_mcmc.py
+++ src/structure_learning/approximators/partition_mcmc.py
@@ -76,6 +76,7 @@ class PartitionMCMC(MCMC):
                     self.initial_state = self._pc_state.incidence.astype(int) - self.initial_state.incidence.astype(int) 
                     self.initial_state[self.initial_state < 0] = 0
                     self.initial_state = self.initial_state.astype(bool)
+                    self.initial_state = DAG(incidence=self.initial_state, nodes=self.node_labels)
                 if whitelist is not None:
                     self.initial_state.incidence[whitelist > 0] = True
                 if blacklist is not None:
